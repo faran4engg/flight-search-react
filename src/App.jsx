476a14d85b214268f5c77649c9b25daa90c04f88
faran4engg <faran4engg@gmail.com>
2020-07-18 21:40:12 +0200

    material ui + grid adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import "./App.css";
+import Container from "@material-ui/core/Container";
 import ResultArea from "./containers/ResultArea/ResultArea";
 import SearchArea from "./containers/SearchArea/SearchArea";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <SearchArea />
-      <ResultArea />
-    </div>
+    <Container>
+      <div className="App">
+        <SearchArea />
+        <ResultArea />
+      </div>
+    </Container>
   );
 }
 
--- src/containers/SearchArea/SearchArea.jsx
+++ src/containers/SearchArea/SearchArea.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./SearchArea.css";
 const SearchArea = () => {
-  return <div className="search">SearchArea</div>;
+  return (
+    <div className="search">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
 
 export default SearchArea;
