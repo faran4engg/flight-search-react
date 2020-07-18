a6b0d34efbe057cc10efa6164873f309de4f6ae1
faran4engg <faran4engg@gmail.com>
2020-07-18 22:05:39 +0200

    adding problem statement and cleaning mess

--- src/containers/SearchArea/SearchArea.jsx
+++ src/containers/SearchArea/SearchArea.jsx
@@ -1,14 +1,39 @@
 import React from "react";
-import "./SearchArea.css";
+import { Form, Field } from "react-final-form";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
 const SearchArea = () => {
+  const onSubmit = (values) => {
+    console.log("onSubmit...", values);
+  };
   return (
-    <div className="search">
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </div>
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, form, invalid }) => (
+        <form noValidate onSubmit={handleSubmit}>
+          <Field name={"das"} key={"das"}>
+            {({ input, meta }) => (
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  fullWidth
+                  id="outlined-basic"
+                  label="Outlined"
+                  variant="outlined"
+                />
+              </Grid>
+            )}
+          </Field>
+
+          <div className="buttons">
+            <Button variant="contained" color="primary">
+              Primary
+            </Button>
+          </div>
+        </form>
+      )}
+    />
   );
 };
 
