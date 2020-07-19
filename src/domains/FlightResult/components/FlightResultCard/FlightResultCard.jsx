6f89baf26cde4d11bf3d3fef39da96916cc82b8b
faran4engg <faran4engg@gmail.com>
2020-07-19 22:28:42 +0200

    adding FlightResultCard

---
+++ src/domains/FlightResult/components/FlightResultCard/FlightResultCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import airplane1 from "assets/svg/airplane4.svg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  content: {
+    flex: "1 0 auto",
+  },
+  cover: {
+    width: 140,
+    transform: "scale(0.9)",
+  },
+}));
+
+const FlightResultCard = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <h3>Live From Space</h3>
+          <h4>Mac Miller</h4>
+        </CardContent>
+      </div>
+
+      <CardMedia
+        className={classes.cover}
+        image={airplane1}
+        title="Live from space album cover"
+      />
+    </Card>
+  );
+};
+export default FlightResultCard;
--- src/pages/FlightApp/Areas/ResultArea/ResultArea.jsx
+++ src/pages/FlightApp/Areas/ResultArea/ResultArea.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import noFlightImg from "assets/svg/no-flights.svg";
+import FlightResultCard from "domains/FlightResult/components/FlightResultCard/FlightResultCard";
 const ResultArea = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <img alt="no-flights" src={noFlightImg} width="100" height="100" />
       <h3>Oops, No Flights Found</h3>
+      <FlightResultCard />
     </div>
   );
 };
