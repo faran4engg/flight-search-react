ba8d933ba81e49b1bedf887527115fbdb2c20d90
faran4engg <faran4engg@gmail.com>
2020-07-19 20:45:28 +0200

    no result svg

--- src/pages/FlightApp/Areas/ResultArea/ResultArea.jsx
+++ src/pages/FlightApp/Areas/ResultArea/ResultArea.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-
+import noFlightImg from "assets/svg/no-flights.svg";
 const ResultArea = () => {
   return (
-    <div>
-      <ul>
-        <li>Result 1</li>
-        <li>Result 2</li>
-        <li>Result 3</li>
-      </ul>
+    <div style={{ textAlign: "center" }}>
+      <img alt="no-flights" src={noFlightImg} width="100" height="100" />
+      <h3>Oops, No Flights Found</h3>
     </div>
   );
 };
